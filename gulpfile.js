d7b654ef7bbfb2e9ca48d0c3dbef56e5c678e298
Daniel Tael <daniel.tael@ottoboni.se>
2014-11-14 09:24:42 +0100

    Share module

--- gulpfile.js
+++ gulpfile.js
@@ -124,11 +124,11 @@ gulp.task('styles', function() {
             sass: 'src/sass'
         }))
         //.pipe(autoprefixer())
-        .pipe(autoprefixer('last 3 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'Firefox >= 10', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(autoprefixer('last 3 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'Firefox >= 22', 'opera 12', 'ios 6', 'android 4'))
         //.pipe(autoprefixer('last 3 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'Firefox >= 10', 'opera 12.1', 'ios 6', 'android 4'))
         //.pipe(gulp.dest(paths.styles.dest))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(minifycss())
+        //.pipe(rename({ suffix: '.min' }))
+        //.pipe(minifycss())
         .pipe(gulp.dest(paths.styles.dest));
 });
 gulp.task('sprite', function() {
